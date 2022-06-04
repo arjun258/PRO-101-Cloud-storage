938adaefec9a321e1f3ee4926dd57b6225f43bcf
arjun258 <66076537+arjun258@users.noreply.github.com>
2022-06-04 13:25:51 +0530

    files arent uploading to dropbox and a warning

--- uploadFiles.py
+++ uploadFiles.py
@@ -1,46 +1,35 @@
-import dropbox
 import os
-class UploadFiles:
-       def __init__(self, access_token,upload_file):
-        self.access_token = access_token
-        self.upload_file = upload_file
-     
+import dropbox
 
-def uploadFiles(self, file_from, file_to,local_path,upload_file):
-    for root, dirs, files in os.walk(file_from):
-        relative_path = os.path.relpath(local_path , file_from)
-        dropbox_path = os.path.join(file_to, relative_path)
+
+
+
+class TransferData:
+    def __init__(self,access_token):
+        self.access_token =  access_token
+
+    def uploadFile(self, file_from, file_to):
         dbx = dropbox.Dropbox(self.access_token)
+        for root, dirs, files in os.walk(file_from):
 
-        with open(file_from, 'rb') as f:
-            dbx.files_upload(f.read(), file_to)
-
+            for filename in files:
+                local_path = os.path.join(root, filename)                   
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+                  
+                with open(local_path, 'rb') as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
 
 def main():
-    access_token = 'sl.BIzzYYXQNLCzdR8RA4nmfyfiXSZqFvafpsr5__7OrjrJPaxnJnO1FPH0SxY_c2iOAGGCUL6FN3yL2DGvxxi6iIEYa9tOQwg_CPu3mJxZ3IzPfkdOF__kl11UJli2uIXjjwQHVftQ'
-    transferData = UploadFiles(access_token)
+    access_token = 'sl.BI6Z9VE32y15TlqyUxm_-F0pXFrDs5Hh8079ECX-qHTrjokXsxGsKBRDfxUN6Q1pyHOD5SlIAy0TJNvSjCSR7EGzKLn7-11-9F2lWU7X--nEGhDgfK_W1Gw9eRn8iUTIFeLfELFW'
+    transferData = TransferData(access_token)
 
-    file_from = 'C101.py'
-    file_to = '/python/C105.py'  # The full path to upload the file to, including the file name
+    file_from = 'pro2.py'
+    file_to = '/python/pro2.py'  
 
-    # API v2
-    transferData.upload_file(file_from, file_to)
+ 
+    transferData.uploadFile(file_from,file_to)
+    print("moved")
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-           
- 
-
-      
-      
-      
-      
-      
-
-
